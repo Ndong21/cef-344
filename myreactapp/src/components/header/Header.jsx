987f28937b8764e8051aabbc787807773d0919b8
Ndong Henry Ndang <ndonghenryndang@gmail.com>
2023-06-04 10:03:57 +0100

    all frontend sections

---
+++ myreactapp/src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import "./header.css";
+import "./CTA";
+import CTA from "./CTA"; //herewe import the CTA component inside our header
+import me from "../../assets/we2.jpg";
+import HeaderSocial from "./HeaderSocial"; //import of headerSocial component
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>NDONG</h1>
+        <h5 className="text-light">Data Analyst </h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="my_image">
+          <img src={me} alt="my image" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+      </div>
+    </header>
+  );
+};
+export default Header;
