056f62591bd4185efa3df5c16605e4359b401c6e
Ndong Henry Ndang <ndonghenryndang@gmail.com>
2023-06-22 12:52:58 +0100

    about.jsx updated

--- myreactapp/src/components/about/About.jsx
+++ myreactapp/src/components/about/About.jsx
@@ -21,32 +21,28 @@ const About = () => {
               <article className="about__card">
                 <FaAward className="about__icon" />
                 <h5>Experience</h5>
-                <small>1+ Years</small>
+                <small>4+ Years</small>
               </article>
               <article className="about__card">
                 <FiUsers className="about__icon" />
                 <h5>Clients</h5>
-                <small>3 world widely</small>
+                <small>Over 95 world widely</small>
               </article>
               <article className="about__card">
                 <VscFolderLibrary className="about__icon" />
                 <h5>Projects</h5>
-                <small>2 Completed</small>
+                <small>70 Completed</small>
               </article>
             </div>
             <p>
-              I am a young software engineer trained at the Faculty of
-              Engineering and Technology(FET) of the University of Buea . I get
-              my B.eng in 2021. Being on demand for experience, I apply to
-              Camsol.io which accepts me and gives me the chance to work on real
-              business projects in order to gain experience. From July 2021 to
-              today, I learned technologies such as react, react native, in
-              short the MERN stack, I am still learning and counting on the
-              support of Camsol to increase my knowledge at end of being an
-              engineer not only graduate, but also qualified. I hold a
-              French-speaking baccalaureate D sexion, my university studies at
-              FET I did in English. I can therefore express myself in French and
-              in English (average).
+              I am a young data analyst with a degree in software engineering. I work with businesses and individuals to provide 
+              tech and business solutions using data by performing data analysis, data analytics, building machine learning 
+              models and simple easy visualisations in form of dashboards and reports (using python, sql and power BI) for 
+              exectives and investors to enhance decision making. I also carry out market research and consultancy for 
+              businesses to assess the performance of the business or a certain product.
+              As a software engineer, proficient in both frontend and backend technologies such as react, flask, fastapi, I build responsive 
+              web and mobile applications for my clients. This also helps me build end to end machine learning projects 
+              for my clients. I am fluent in both english and french and i can work in both remote and onsite settings
             </p>
             <a href="#contact" className="btn btn-primary">
               Let's talk
