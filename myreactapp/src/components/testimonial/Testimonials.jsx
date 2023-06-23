033fd66638e366bb4338813db0991d469ee9a50e
Ndong Henry Ndang <ndonghenryndang@gmail.com>
2023-06-23 18:43:07 +0100

    backend connection

--- myreactapp/src/components/testimonial/Testimonials.jsx
+++ myreactapp/src/components/testimonial/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './testimonials.css';
+import './testimonial.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import 'swiper/css';
