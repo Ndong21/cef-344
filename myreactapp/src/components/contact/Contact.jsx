e046e3945ded9208ee39242a7706a8fe15529079
Ndong Henry Ndang <ndonghenryndang@gmail.com>
2023-06-24 10:56:02 +0100

    email.js debugged

--- myreactapp/src/components/contact/Contact.jsx
+++ myreactapp/src/components/contact/Contact.jsx
@@ -1,95 +1,52 @@
 import React from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {AiOutlineLinkedin} from 'react-icons/ai'
-import {BsWhatsapp} from 'react-icons/bs'
-import {useRef} from 'react'
-import emailjs from 'emailjs-com'
+import { BsWhatsapp } from 'react-icons/bs';
+import {MdOutlineMailOutline} from 'react-icons/md';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-const Contact = () => {
+function Contact() {
   const form = useRef();
 
   const sendEmail = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      emailjs.sendForm('service_5vuauza', 'template_fdnnl56', form.current, 'EmSdUNQzQuxkUqGWx')
-      e.target.reset() 
+    emailjs.sendForm('service_qul0l2o', 'template_txtcwps', form.current, 'eonG-scVJiYuHgGC1')
       .then((result) => {
-        console.log(result.text)
-      },(error) =>{
-        console.log(error.text);
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
       });
   };
-    return (
-      <section id="contact">
-        <h5>Get in touch</h5>
-        <h2>Contact me</h2>
-
-        <div className="container contact__container">
-          <div className="contact__options">
-            <article className="contact__option">
-              <MdOutlineEmail className="contact__option__icon" />
-              <h4>Email</h4>
-              <h5>ndonghenryndang@gmail.com</h5>
-              <a
-                href="mailto:alainshiress64@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                send a message
-              </a>
-            </article>
-
-            <article className="contact__option">
-              <AiOutlineLinkedin className="contact__option__icon" />
-              <h4>LinkedIn</h4>
-              <h5>Profile</h5>
-              <a
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Tape me on LinkedIn
-              </a>
-            </article>
-
-            <article className="contact__option">
-              <BsWhatsapp className="contact__option__icon" />
-              <h4>Whatsapp</h4>
-              <h5>Direct message</h5>
-              <a
-                href="https://web.whatsapp.com/send?phone=+237672614379"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Whatsapp me
-              </a>
-            </article>
-          </div>
-
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name="name" placeholder="full name" required />
-            <input
-              type="email"
-              name="email"
-              placeholder="your email"
-              required
-            />
-            <textarea
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-              placeholder="your message"
-              required
-            ></textarea>
-            <button type="submit" className="btn btn-primary">
-              Send
-            </button>
-          </form>
+  return (
+    <section id='contact'>
+     
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className="contact__option">
+            <MdOutlineMailOutline className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>ndangndonghenry@gmail.com</h5>
+            <a href='mailto:ndangndonghenry@gmail.com'>Send a message </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>whatsapp</h4>
+            <h5>+237678091386</h5>
+            <a href='https://wa.me/+237678091386'>Send a message </a>
+          </article>
         </div>
-      </section>
-    );
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder="Name"required></input>
+          <input type="email" name='email' placeholder="email"required></input>
+          <textarea name='message' placeholder="Message" required></textarea>
+          <button type="submit"className='btn btn primary'>send Message</button>
+        </form>
+        </div>
+    </section>
+  )
 }
 
 export default Contact
